47ee050cad0bf70b61a114d47938bdfe00c46e0a
Nathan Geffen <nathangeffen@gmail.com>
2020-06-14 13:20:09 +0200

    Updated docs.

--- macro.py
+++ macro.py
@@ -1,4 +1,4 @@
-"""Compartmental or macro modelling of infectious diseases.
+'''Compartmental or macro modelling of infectious diseases.
 
 This module provides an API for macro or compartmental models of
 infectious disease epidemics.
@@ -8,9 +8,295 @@ showing changes to the models. This output can be converted to
 a flat table or exported to a CSV file.
 
 The main functions are:
-    * simulate - Takes a list of model specifications and ex
-    * simulateSeries - multiple lists of model specifications and
-                       runs them in parallel.
+    * simulate - Takes a list of model specifications and executes it.
+    * simulate_series - Multiple lists of model specifications and
+                        runs them in parallel.
+    * series_to_table - Takes the output of the above and
+                        converts to flat list of lists, with
+                        each entry representing output for one iteration
+                        of a group.
+    * series_to_csv - Same as above except outputs a csv file.
+
+A model specification consists of groups. A model is itself the
+top-level group.
+
+Each group can itself contain groups.
+
+A group specification is a dictionary containing the following
+one or more of the following fields:
+
+    * name - Name of group.
+    * transitions - Dictionary of probabilities of
+                    transitioning from one compartment to another.
+    * compartments - Dictionary of the number of people per
+                     compartment. A group must either have a
+                     "compartments" entry or "groups" entry but
+                     not both.
+    * groups: - Array of groups within this group.
+    * parameters: - Hyper-parameters for the model. These are usually
+                    only specified in the top-level (model) group. If
+                    not specified then the default PARAMETERS
+                    are copied and used in the model.
+                    If the hyper-parameters are partially specified,
+                    the unspecified parameters are copied from PARAMETERS.
+
+Here are two examples:
+
+This is a very simple model.
+
+{
+    'name': 'Simple model',
+    'compartments': {
+        'S': 57000000,
+        'I': 1,
+        'R': 0
+     },
+     'transitions': {
+        'S_I': 0.6,
+        'I_R': 0.1
+     },
+}
+
+This is complicated one showing many of the features of a model specification.
+
+{
+    'name': 'South African Covid-19',
+    'parameters': macro.make_parameters({
+        'to': 365,
+        'record_frequency': 1,
+        'record_last': False,
+        'noise': 0.1,
+        'asymptomatic_infectiousness': 0.75,
+        'reduce_infectivity': 0.999,
+        'after_funcs': [macro.reduce_infectivity, ],
+        'transition_funcs': {
+            'S_E': macro.delta_S_I1,
+        }
+    }),
+    'transitions': {
+        'S_E': 0.31,
+        'E_A': 0.125,
+        'E_Im': 0.125,
+        'Im_Ic': 0.2,
+        'Ic_R': 0.2,
+        'A_R': 0.2
+    },
+    'groups': [
+        {
+            'name': 'Urban informal',
+            'transitions': {
+                'S_E': 0.45,
+            },
+            'groups': [
+                {
+                    'name': '0-24',
+                    'transitions': {
+                        'E_A': 0.25,
+                        'E_Im': 0.01,
+                        'Ic_D': 0.002
+                    },
+                    'compartments': {
+                        'S': 2100000,
+                        'E': 10,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '25-54',
+                    'transitions': {
+                        'Ic_D': 0.003
+                    },
+                    'compartments': {
+                        'S': 2100000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '55-',
+                    'transitions': {
+                        'Ic_D': 0.03
+                    },
+                    'compartments': {
+                        'S': 600000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                }
+            ]
+        },
+        {
+            'name': 'Urban formal',
+            'groups': [
+                {
+                    'name': '0-24',
+                    'transitions': {
+                        'E_A': 0.25,
+                        'E_Im': 0.01,
+                        'Ic_D': 0.002
+                    },
+                    'compartments': {
+                        'S': 16940000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '25-54',
+                    'transitions': {
+                        'Ic_D': 0.003
+                    },
+                    'compartments': {
+                        'S': 16940000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '55-',
+                    'transitions': {
+                        'Ic_D': 0.03
+                    },
+                    'compartments': {
+                        'S': 4620000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                }
+            ]
+        },
+        {
+            'name': 'Rural',
+            'transitions': {
+                'S_E': 0.2,
+            },
+            'groups': [
+                {
+                    'name': '0-24',
+                    'transitions': {
+                        'E_A': 0.25,
+                        'E_Im': 0.01,
+                        'Ic_D': 0.002
+                    },
+                    'compartments': {
+                        'S': 7260000,
+                        'E': 10,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '25-54',
+                    'transitions': {
+                        'Ic_D': 0.003
+                    },
+                    'compartments': {
+                        'S': 7260000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                },
+                {
+                    'name': '55-',
+                    'transitions': {
+                        'Ic_D': 0.03
+                    },
+                    'compartments': {
+                        'S': 2000000,
+                        'E': 0,
+                        'Im': 0,
+                        'Ic': 0,
+                        'A': 0,
+                        'R': 0,
+                        'D': 0
+                    }
+                }
+
+            ]
+        }
+    ]
+}
+
+
+A ModelList is a list of models. The "simulate" function takes
+a list of models as its first parameter. Why? Let's say you have
+distinct regions you'd like to model, say London and Manchester.
+You would specify a model for each and then put them in a list (i.e. you
+now have a ModelList data structure). You can then run "simulate"
+on this ModelList.
+
+The output is two distinct sets of independent new model specification
+with updated compartments on each iteration.  i.e. a list of ModelLists.
+
+This is called a ModelListSeries.
+
+If the output of each of the models is independent, what's the use
+of putting them in a ModelList? Well, you could specify a custom
+function in the "after_funcs" hyper parameter of one of the models. This
+function could migrate individuals between the two models or connect
+them in some other way.
+
+
+Transitions
+-----------
+
+A transition name consists of two compartment names separated by
+an underscore. The default PARAMETERS consist of a dictionary called
+'transition_funcs' which you can override in the model's parameters.
+The transition function to execute is looked up in this dictionary. If
+it's not found the function specified by 'default' is executed.
+
+For most transitions, a proportion of the "from" compartment is moved
+to the "to" compartment. The delta_X_Y function takes care of this.
+
+But for new infections you almost always want
+a standard SIR-like equations such as this:
+
+delta = susceptibles * number of contacts per iteration *
+                       risk of infection per contact *
+                       total number of infectious individuals /
+                       total population
+
+susceptibles -= delta
+infectious (or exposed) individuals += delta
+
+Two functions are provided to deal with this: the very simple delta_S_I
+and the more sophisticated (but slower) delta_S_I1.
+
+Some of the compartment name prefixes are meaningful. A compartment
+name generally starts with one of these meaningful prefixes and then
+a unique identifier. E.g. I1, I2, I3 or I4 for various stages of infection.
 
 Meaningful compartment prefixes
 -------------------------------
@@ -18,7 +304,7 @@ Meaningful compartment prefixes
 S - Susceptible  (See delta_S_I and delta_S_I1)
 E - Exposed  (See delta_S_I and delta_S_I1)
 I - Infectious (See delta_S_I and delta_S_I1)
-A - Asymptomatic
+A - Asymptomatic (See delta_S_I1 and asymptomatic_infectiousness)
 T - On treatment (See delta_S_I1 and treatment_infectiousness)
 N - Population size. Strictly reserved. Do not prefix any compartment N.
 D - Dead (not included in totalling N)
@@ -27,7 +313,7 @@ R - Recovered
 M - Maternal immunity
 V - Vaccinated
 
-"""
+'''
 
 import csv
 from copy import deepcopy
@@ -41,12 +327,25 @@ ModelList = List[Model]
 ModelListSeries = List[ModelList]
 
 
-def delta_S_I(from_to, prod, compartments, totals, model=None):
+def delta_S_I(from_to, beta, compartments, totals, model=None):
+    '''Returns number of new infections.
+
+    Parameters:
+    from_to: (str): transition name consisting of two compartment names
+                    separated by an underscore (e.g. S_Ic)
+    prop: (float): effective contact rate
+    compartments (dict): dictionary of compartments including the two
+                         specified in from_to
+    totals (dict): dictionary containing a key 'N' that is the sum of the
+                   total population for this model.
+    '''
     from_, to_ = from_to.split("_")
-    return prod * compartments[from_] * totals[to_] / totals['N']
+    return beta * compartments[from_] * totals[to_] / totals['N']
 
 
 def delta_X_Y(from_to, prod, compartments, totals, model=None):
+    '''
+    '''
     from_, _ = from_to.split("_")
     return prod * compartments[from_]
 
